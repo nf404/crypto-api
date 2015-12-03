9855f836f1d285c35782ac5e39b9c0941ba7e1a4
mogilnikov <mogilnikov-av@fxclub.org>
2015-12-03 23:14:44 +0100

    Add message processing & add md2 hasher

--- crypto-api.js
+++ crypto-api.js
@@ -1,7 +1,26 @@
-(function (root) {
+(/**
+ *
+ * @param {Object} root
+ * @returns {CryptoApi}
+ */
+    function (root) {
     'use strict';
 
-    var HasherInterface = function HashAlgorithm(options) {};
+    var HasherInterface = function HasherInterface(options) {
+        this.message = [];
+        this.length = 0;
+        this.state.options = options;
+    };
+    /**
+     * Message
+     * @type {number[]}
+     */
+    HasherInterface.prototype.message = [];
+    /**
+     * Length of message
+     * @type {number}
+     */
+    HasherInterface.prototype.length = 0;
     /**
      *  Size of unit in bytes (4 = 32 bits)
      * @type {number}
@@ -30,10 +49,9 @@
     HasherInterface.prototype.processBlock = function processBlock(block) {};
     /**
      * Process last block and return hash
-     * @param {number[]} block
      * @return {number[]} hash
      */
-    HasherInterface.prototype.finalize = function finalize(block) {};
+    HasherInterface.prototype.finalize = function finalize() {};
     /**
      * Return current state
      * @returns {Object}
@@ -48,17 +66,59 @@
     HasherInterface.prototype.setState = function setState(state) {
         this.state = state;
     };
+    /**
+     * Update message
+     * @param {string|number[]} message
+     */
+    HasherInterface.prototype.update = function update(message) {
+        if (typeof message == 'string') {
+            message = unescape(encodeURIComponent(message)); // @todo Change to something fast and not deprecated
+            this.length += message.length;
+            for (var i = 0, msgLen = message.length; i < msgLen; i++) {
+                this.message.push(message.charCodeAt(i));
+            }
+        } else {
+            this.length += message.length;
+            this.message = this.message.concat(message);
+        }
+        this.process();
+    };
+    /**
+     * Process ready blocks
+     */
+    HasherInterface.prototype.process = function process() {
+        while (this.message.length >= this.blockSize * this.unitSize) {
+            var j=0, b=0, block = this.message.splice(0, this.blockSize * this.unitSize);
+            if (this.unitSize > 1) {
+                var blockUnits = [];
+                for (var i = 0, u = 0; i < block.length; i += this.unitSize, u++) {
+                    if (this.unitOrder == 1) {
+                        for (j = this.unitSize - 1, b = 0; j >= 0; j--, b += 8) {
+                            blockUnits[u] |= (block[i + j] << b);
+                        }
+                    } else {
+                        for (j = 0, b = 0; j < this.unitSize; j++, b += 8) {
+                            blockUnits[u] |= (block[i + j] << b);
+                        }
+                    }
+                }
+                this.processBlock(blockUnits);
+            } else {
+                this.processBlock(block);
+            }
+        }
+    };
 
-    var Api = function Api () {};
-    Api.prototype.HasherInterface = HasherInterface;
-    Api.prototype.hashers = {};
+    var CryptoApi = function CryptoApi () {};
+    CryptoApi.prototype.HasherInterface = HasherInterface;
+    CryptoApi.prototype.hashers = {};
     /**
      * Get new hasher object
      * @param {string} algo
      * @param {Object} options
      * @returns {HasherInterface}
      */
-    Api.prototype.hasher = function hasher(algo, options) {
+    CryptoApi.prototype.hasher = function hasher(algo, options) {
         /** @type HasherInterface */
         var hasher = this.hashers[algo];
         if (hasher === undefined || (!hasher instanceof HasherInterface)) {
@@ -67,5 +127,6 @@
         return new hasher(options);
     };
 
-    return new Api();
+    root.CryptoApi = new CryptoApi();
+    return root.CryptoApi;
 })(this);
