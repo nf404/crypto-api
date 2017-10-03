5416442288d03699f957ce3b62749ea1806d7002
nf404 <nf404.name@gmail.com>
2017-10-03 23:30:30 +0200

    rename & add base encoder class

---
+++ src/encoder.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class Encoder {
+  constructor () {
+  }
+
+  /**
+   *
+   * @param {string} raw
+   * @returns {string}
+   */
+  encode(raw) {
+    return raw;
+  }
+}
+
+export default Encoder
