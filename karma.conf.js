1bddc4f6b6ca4a0748c86f5bc854ca7981f36b42
nf404 <nf404.name@gmail.com>
2017-10-09 12:35:37 +0200

    fix istanbul coverage

--- karma.conf.js
+++ karma.conf.js
@@ -5,34 +5,44 @@ module.exports = function (config) {
     singleRun: true,
     frameworks: ['mocha'],
     files: [
+      'src/*.js',
+//      'test/*.js',
       'test/test.js'
-    ],/*
-    plugins: [
-      'karma-coverage',
-      'karma-mocha',
-      'karma-phantomjs-launcher',
-      'karma-mocha-reporter'
-    ],*/
+    ],
     browsers: ['PhantomJS'],
-    reporters: ['progress', 'coverage', 'mocha'],
+    reporters: ['mocha'], // 'progress', 'coverage', 'coverage-istanbul',
     preprocessors: {
-      'src/*.js': ['webpack'], // 'coverage',
-      'test/*.js': ['webpack']
+      'src/*.js': ['webpack'], // , 'coverage'
+      'test/*.js': ['webpack'] // , 'sourcemap'
     },
     coverageReporter: {
-      dir: 'coverage/',
+      dir: 'coverage',
       reporters: [
         {type: 'lcovonly', subdir: 'karma'},
-        {type: 'json', subdir: 'karma'}
+        {type: 'json', subdir: 'karma'},
+        {type: 'html', subdir: 'karma'}
       ]
     },
     webpack: {
       module: {
+/*        rules: [
+          // instrument only testing sources with Istanbul
+          {
+            test: /\.js$/,
+            use: {
+              loader: 'istanbul-instrumenter-loader',
+              options: { esModules: true }
+            },
+            enforce: 'post',
+            exclude: /node_modules|\.spec\.js$/ //,            include: require('path').resolve('src/')
+          }
+        ],*/
         loaders: [
           {
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader?presets[]=es2015'
+            loader: 'babel-loader?presets[]=es2015',
+//            options: { sourceMap: 'inline', plugins: ['istanbul'] },
           }
         ]
       },
